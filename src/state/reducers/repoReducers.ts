88e83957d96a360609caa8d2e50da52758916e10
Truong Dao <ton.truong.dao@outlook.com>
2021-05-16 16:36:32 -0400

    Added TS on Reducer Output

--- src/state/reducers/repoReducers.ts
+++ src/state/reducers/repoReducers.ts
@@ -4,10 +4,11 @@ interface RepoState {
   data: string []
 }
 
-const reducer = (state: RepoState, action: any) => {
+// Will Always Need To Match The Interface
+const reducer = (state: RepoState, action: any): RepoState => {
   switch (action.type) {
     case 'search_repo':
-      return { loading: true, error: null, data: []}
+      return { loading: true, error: null, data: [] }
     case 'search_repo_success':
       return { loading: false, error: null, data: action.payload }
     case 'search_repo_error':
