8603273bfdba937b52386eea54d4ba1f6b56419c
Truong Dao <truong@sinistar.ca>
2021-05-18 18:45:09 -0400

    Created ActionCreator

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import { ActionType } from '../action-type'
+import { Action } from '../actions'
+
+export const searchRepo = (term: string) => {
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionType.SEARCH_REPO
+    })
+
+    try {
+      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
+        params: {
+          text: term
+        }
+      })
+
+      const names = data.map((result: any) => {
+        return result.package.name
+      })
+
+      dispatch({
+        type: ActionType.SEARCH_REPO_SUCCESS,
+        payload: names
+      })
+
+    } catch (err) {
+      dispatch({
+        type: ActionType.SEARCH_REPO_ERROR,
+        payload: err.message
+      })
+    }
+  }
+}
