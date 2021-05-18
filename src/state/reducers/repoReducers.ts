07dadcae8cf4326809812cda2b885c3701380395
Truong Dao <ton.truong.dao@outlook.com>
2021-05-18 09:34:58 -0400

    Refactored Action & ActionTypes

--- src/state/reducers/repoReducers.ts
+++ src/state/reducers/repoReducers.ts
@@ -1,29 +1,10 @@
+import { ActionType } from '../action-type'
+import { Action } from '../actions'
+
 interface RepoState {
   loading: boolean;
   error: string | null;
   data: string []
-}
-
-interface SearchRepoAction {
-  type: ActionType.SEARCH_REPO
-}
-
-interface SearchRepoSuccessAction {
-  type: ActionType.SEARCH_REPO_SUCCESS
-  payload: string[]
-}
-
-interface SearchRepoErrorAction {
-  type: ActionType.SEARCH_REPO_ERROR
-  payload: string
-}
-
-type Action =  SearchRepoAction | SearchRepoSuccessAction | SearchRepoErrorAction 
-
-enum ActionType {
-  SEARCH_REPO = 'search_repo',
-  SEARCH_REPO_SUCCESS = 'search_repo_success',
-  SEARCH_REPO_ERROR = 'search_repo_error'
 }
 
 // Will Always Need To Match The Interface
