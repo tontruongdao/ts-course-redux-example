914e017f6b8b3d390f61733ab6142cbe6eaddbd8
tontruongdao <ton.truong.dao@outlook.com>
2021-05-23 20:28:30 -0400

    Rendered Repo List

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,35 +1,37 @@
-import axios from 'axios'
-import { Dispatch } from 'redux'
-import { ActionType } from '../action-type'
-import { Action } from '../actions'
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionType } from '../action-type';
+import { Action } from '../actions';
 
 export const searchRepo = (term: string) => {
   return async (dispatch: Dispatch<Action>) => {
     dispatch({
-      type: ActionType.SEARCH_REPO
-    })
+      type: ActionType.SEARCH_REPO,
+    });
 
     try {
-      const { data } = await axios.get('https://registry.npmjs.org/-/v1/search', {
-        params: {
-          text: term
+      const { data } = await axios.get(
+        'https://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term,
+          },
         }
-      })
+      );
 
-      const names = data.map((result: any) => {
-        return result.package.name
-      })
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
 
       dispatch({
         type: ActionType.SEARCH_REPO_SUCCESS,
-        payload: names
-      })
-
+        payload: names,
+      });
     } catch (err) {
       dispatch({
         type: ActionType.SEARCH_REPO_ERROR,
-        payload: err.message
-      })
+        payload: err.message,
+      });
     }
-  }
-}
+  };
+};
